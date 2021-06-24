326d7f7f2da3b4077dfd4e03684cb044bc446e69
Brett <colbr243@gmail.com>
2021-06-24 18:18:25 +1200

    Tooltips now fade in and out

--- src/components/HoverTooltip/HoverTooltip.tsx
+++ src/components/HoverTooltip/HoverTooltip.tsx
@@ -1,5 +1,6 @@
 //- React Imports
 import { useState } from 'react';
+import { Transition } from 'react-transition-group';
 
 //- Style Imports
 import styles from './HoverTooltip.module.css';
@@ -11,6 +12,23 @@ type props = {
 	style?: React.CSSProperties;
 };
 
+//- Transition Setup
+// I decided to use transition-group rather than CSS because
+// I wanted a simple way to remove from DOM after animation is complete
+const duration = 150;
+
+const defaultStyle: any = {
+	transition: `opacity ${duration}ms ease-in-out`,
+	opacity: 0,
+};
+
+const transitionStyles: any = {
+	entering: { opacity: 0, display: 'block' },
+	entered: { opacity: 1, display: 'block' },
+	exiting: { opacity: 0, display: 'block' },
+	exited: { opacity: 0, display: 'none' },
+};
+
 const HoverTooltip: React.FC<props> = ({
 	tooltipContent,
 	tooltipStyle,
@@ -18,7 +36,10 @@ const HoverTooltip: React.FC<props> = ({
 	style,
 }) => {
 	const [isActive, setIsActive] = useState(false);
-	const open = () => setIsActive(true);
+
+	const open = () => {
+		setIsActive(true);
+	};
 	const close = () => setIsActive(false);
 
 	return (
@@ -29,11 +50,21 @@ const HoverTooltip: React.FC<props> = ({
 			onMouseLeave={close}
 		>
 			{children}
-			{isActive && (
-				<div style={tooltipStyle} className={styles.Tooltip}>
-					{tooltipContent}
-				</div>
-			)}
+
+			<Transition in={isActive} timeout={duration}>
+				{(state) => (
+					<div
+						style={{
+							...defaultStyle,
+							...transitionStyles[state],
+							...tooltipStyle,
+						}}
+						className={styles.Tooltip}
+					>
+						{tooltipContent}
+					</div>
+				)}
+			</Transition>
 		</div>
 	);
 };
